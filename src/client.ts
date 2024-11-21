aaa7b039fe2e2ded5659f95bf485d341d94d7c9d
liuhaifeng <liuhaifeng@sensetime.com>
2024-11-21 15:33:46 +0800

    upgrade(other):

--- src/client.ts
+++ src/client.ts
@@ -4,150 +4,155 @@ import { WsClient, Socket } from './typings';
 import { uuid } from './lib';
 
 export class WebsocketClient implements WsClient.Client {
-    private webSocket: WebSocket;
-    private addr: string | URL;
-    private configs: WebSocket.ClientOptions | ClientRequestArgs;
-    private options: WsClient.Options = { timeout: 10 };
-    private record: Record<string, { result: Socket.MethodResponse['result'], error: Socket.MethodResponse['error'] }> = {};
+	private webSocket: WebSocket;
+	private addr: string | URL;
+	private configs: WebSocket.ClientOptions | ClientRequestArgs;
+	private options: WsClient.Options = { timeout: 10 };
+	private record: Record<string, { result: Socket.MethodResponse['result'], error: Socket.MethodResponse['error'] }> = {};
 
-    constructor(address: string | URL, configs?: WebSocket.ClientOptions | ClientRequestArgs, options?: WsClient.Options) {
-        this.addr = address;
-        if (configs) {
-            this.configs = configs;
-        }
-        if (options) {
-            if (options.timeout) {
-                this.options.timeout = options.timeout;
-            }
-        }
-    }
+	constructor(address: string | URL, configs?: WebSocket.ClientOptions | ClientRequestArgs, options?: WsClient.Options) {
+		this.addr = address;
+		if (configs) {
+			this.configs = configs;
+		}
+		if (options) {
+			if (options.timeout) {
+				this.options.timeout = options.timeout;
+			}
+		}
+	}
 
-    private getRecordKey(method: string, id: string) {
-        return `${method}-${id}`;
-    }
+	private getRecordKey(method: string, id: string) {
+		return `${method}-${id}`;
+	}
 
-    async open() {
-        const self = this;
+	async open() {
+		const self = this;
 
-        await new Promise((resolve, reject) => {
-            this.webSocket = new WebSocket(this.addr, this.configs);
-            this.webSocket.on('message', data => {
-                const res = JSON.parse(data.toString()) as Socket.MethodResponse;
-                const cacheId = self.getRecordKey(res.method, res.id);
+		await new Promise((resolve, reject) => {
+			this.webSocket = new WebSocket(this.addr, this.configs);
+			this.webSocket.on('message', data => {
+				const res = JSON.parse(data.toString()) as Socket.MethodResponse;
+				const cacheId = self.getRecordKey(res.method, res.id);
 
-                if (self.record[cacheId]) {
-                    if (res.error) {
-                        self.record[cacheId].error = res.error;
-                    } else {
-                        self.record[cacheId].result = res.result;
-                    }
-                } else {
-                    if (res.method) {
-                        self.webSocket.emit(res.method, res.error, res.result);
-                    }
-                }
-            });
-            this.webSocket.on('open', resolve);
-            this.webSocket.on('error', reject);
-        });
-    }
+				if (self.record[cacheId]) {
+					if (res.error) {
+						self.record[cacheId].error = res.error;
+					} else {
+						self.record[cacheId].result = res.result;
+					}
+				} else {
+					if (res.method) {
+						self.webSocket.emit(res.method, res.error, res.result);
+					}
+				}
+			});
+			this.webSocket.on('open', resolve);
+			this.webSocket.on('error', reject);
+		});
+	}
 
-    async request(method: string, params?: unknown): Promise<WsClient.RequestResult> {
-        if (this.status !== WebSocket.OPEN || !method) {
-            return null;
-        }
-        const id = uuid();
-        const cacheId = this.getRecordKey(method, id);
-        const self = this;
-        let result = null;
+	/** 原始websocket客户端实例 */
+	get client() {
+		return this.webSocket;
+	}
 
-        try {
-            result = await new Promise((resolve, reject) => {
-                this.record[cacheId] = new class Cache {
-                    private timer: number;
-                    constructor() {
-                        // eslint-disable-next-line consistent-this, @typescript-eslint/no-this-alias
-                        const _self = this;
+	async request(method: string, params?: unknown): Promise<WsClient.RequestResult> {
+		if (this.status !== WebSocket.OPEN || !method) {
+			return null;
+		}
+		const id = uuid();
+		const cacheId = this.getRecordKey(method, id);
+		const self = this;
+		let result = null;
 
-                        this.timer = setTimeout(() => {
-                            _self.error = {
-                                code: -32001,
-                                message: 'Time out',
-                                data: 'Time out'
-                            };
-                        }, self.options.timeout * 1000) as unknown as number;
+		try {
+			result = await new Promise((resolve, reject) => {
+				this.record[cacheId] = new class Cache {
+					private timer: number;
+					constructor() {
+						// eslint-disable-next-line consistent-this, @typescript-eslint/no-this-alias
+						const _self = this;
 
-                        self.webSocket.send(JSON.stringify({
-                            jsonrpc: '2.0',
-                            id,
-                            method,
-                            params: params || {}
-                        }));
-                    }
+						this.timer = setTimeout(() => {
+							_self.error = {
+								code: -32001,
+								message: 'Time out',
+								data: 'Time out'
+							};
+						}, self.options.timeout * 1000) as unknown as number;
 
-                    private clearTimer() {
-                        if (this.timer) {
-                            clearTimeout(this.timer);
-                            this.timer = null;
-                        }
-                        delete self.record[cacheId];
-                    }
+						self.webSocket.send(JSON.stringify({
+							jsonrpc: '2.0',
+							id,
+							method,
+							params: params || {}
+						}));
+					}
 
-                    set result(data: Socket.MethodResponse['result']) {
-                        this.clearTimer();
-                        resolve({ result: data });
-                    }
+					private clearTimer() {
+						if (this.timer) {
+							clearTimeout(this.timer);
+							this.timer = null;
+						}
+						delete self.record[cacheId];
+					}
 
-                    set error(error: Socket.MethodResponse['error']) {
-                        this.clearTimer();
-                        reject({ error });
-                    }
-                };
-            });
-        } catch (e) {
-            result = e;
-        }
+					set result(data: Socket.MethodResponse['result']) {
+						this.clearTimer();
+						resolve({ result: data });
+					}
 
-        return result;
-    }
+					set error(error: Socket.MethodResponse['error']) {
+						this.clearTimer();
+						reject({ error });
+					}
+				};
+			});
+		} catch (e) {
+			result = e;
+		}
 
-    async ping(): Promise<WsClient.RequestResult> {
-        return await this.request('ping');
-    }
+		return result;
+	}
 
-    listening(method: string, callback: WsClient.ListenCallbackFn, once?: boolean) {
-        if (Boolean(once) === true) {
-            this.webSocket.once(method, callback);
-        } else {
-            this.webSocket.on(method, callback);
-        }
-    }
+	async ping(): Promise<WsClient.RequestResult> {
+		return await this.request('ping');
+	}
 
-    get removeListening() {
-        return this.webSocket.removeListener;
-    }
+	listening(method: string, callback: WsClient.ListenCallbackFn, once?: boolean) {
+		if (Boolean(once) === true) {
+			this.webSocket.once(method, callback);
+		} else {
+			this.webSocket.on(method, callback);
+		}
+	}
 
-    get CONNECTING() {
-        return WebSocket.CONNECTING;
-    }
+	get removeListening() {
+		return this.webSocket.removeListener;
+	}
 
-    get OPEN() {
-        return WebSocket.OPEN;
-    }
+	get CONNECTING() {
+		return WebSocket.CONNECTING;
+	}
 
-    get CLOSING() {
-        return WebSocket.CLOSING;
-    }
+	get OPEN() {
+		return WebSocket.OPEN;
+	}
 
-    get CLOSED() {
-        return WebSocket.CLOSED;
-    }
+	get CLOSING() {
+		return WebSocket.CLOSING;
+	}
 
-    get status() {
-        return this.webSocket.readyState;
-    }
+	get CLOSED() {
+		return WebSocket.CLOSED;
+	}
 
-    close() {
-        this.webSocket.close();
-    }
+	get status() {
+		return this.webSocket.readyState;
+	}
+
+	close() {
+		this.webSocket.close();
+	}
 }
